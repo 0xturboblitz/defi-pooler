e0d3a0fa4371a8b1d2a7d8e4991de443e998076a
nicodlz <nicolas@bangr.app>
2023-02-27 02:26:33 +0100

    gates

--- contracts/l2/PoolerL2.sol
+++ contracts/l2/PoolerL2.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PoolerERC20 is ERC20, Ownable {
+contract PoolerL2 is ERC20, Ownable {
     address usdc;
     address gateway;
 
